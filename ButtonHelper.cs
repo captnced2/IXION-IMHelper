1a1e65ef48323d96b4b6171e71f3e606d1c395e4
captnced <cedric.elsaesser1603@gmail.com>
2025-05-22 19:31:37 +0200

    Added text button Changed original button to icon button

--- ButtonHelper.cs
+++ ButtonHelper.cs
@@ -3,65 +3,50 @@ using System.IO;
 using BulwarkStudios.GameSystems.Ui;
 using BulwarkStudios.Utils.UI;
 using Cpp2IL.Core.Extensions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace IMHelper;
 
-public class ButtonHelper
+public static class ButtonHelper
 {
-    public class Button
+    public abstract class Button(
+        string name,
+        Transform original,
+        Transform parent,
+        Action<Transform> triggerAction,
+        string hoverText = null)
     {
-        private readonly Stream buttonTexture;
-        private readonly string hoverText;
-        private readonly string name;
-        private readonly Transform original;
-        private readonly Transform parent;
-        private readonly Action<Transform> triggerAction;
         public Transform buttonTransform;
         public bool created;
 
-        public Button(string name, Transform original, Transform parent, Action<Transform> triggerAction,
-            Stream buttonTexture,
-            string hoverText = null)
-        {
-            this.name = name;
-            this.hoverText = hoverText;
-            this.parent = parent;
-            this.original = original;
-            this.triggerAction = triggerAction;
-            this.buttonTexture = buttonTexture;
-        }
-
         public void createButton()
         {
-            if (!parent.FindChild(name))
+            if (parent.FindChild(name)) return;
+            var newButton = Object.Instantiate(original, parent);
+            buttonTransform = newButton;
+            newButton.name = name;
+            newButton.GetComponent<UiButtonTriggerUnityEvent>().enabled = false;
+            if (hoverText != null)
             {
-                var newButton = Object.Instantiate(original, parent);
-                buttonTransform = newButton;
-                newButton.name = name;
-                newButton.SetAsLastSibling();
-                newButton.GetComponent<UiButtonTriggerUnityEvent>().enabled = false;
-                if (hoverText != null) newButton.GetComponent<UITooltipHoverHelper>().translatedText = hoverText;
-                newButton.GetComponent<UiButton>()
-                    .add_OnTriggered(new Action(delegate { triggerAction(buttonTransform); }));
-
-                if (buttonTexture != null)
-                {
-                    var oldSprite = buttonTransform.FindChild("Icon").GetComponent<Image>().sprite;
-                    var texture = new Texture2D(2, 2);
-                    texture.LoadImage(buttonTexture.ReadBytes());
-                    var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
-                        oldSprite.textureRectOffset);
-                    buttonTransform.FindChild("Icon").GetComponent<Image>().sprite = sprite;
-                }
-
-                created = true;
-                Plugin.Log.LogInfo("Created button \"" + name + "\" from Assembly \"" +
-                                   Plugin.getCallingAssemblyName() + "\"");
+                var hoverHelper = newButton.GetComponent<UITooltipHoverHelper>();
+                if (hoverHelper != null)
+                    hoverHelper.translatedText = hoverText;
             }
+
+            newButton.GetComponent<UiButton>()
+                .add_OnTriggered(new Action(delegate { triggerAction(buttonTransform); }));
+            createButtonInternal();
+            created = true;
+            var callingAssembly = Plugin.getCallingAssemblyName();
+            if (callingAssembly != null)
+                Plugin.Log.LogInfo("Created button \"" + name + "\" from Assembly \"" +
+                                   callingAssembly + "\"");
         }
+
+        protected abstract void createButtonInternal();
 
         public void hide()
         {
@@ -71,6 +56,42 @@ public class ButtonHelper
         public void show()
         {
             if (created) buttonTransform.gameObject.SetActive(true);
+        }
+    }
+
+    public class TextButton(
+        string name,
+        Transform original,
+        Transform parent,
+        Action<Transform> triggerAction,
+        string text,
+        string hoverText = null)
+        : Button(name, original, parent, triggerAction, hoverText)
+    {
+        protected override void createButtonInternal()
+        {
+            buttonTransform.FindChild("Text (TMP)").GetComponent<TextMeshProUGUI>().text = text;
+        }
+    }
+
+    public class IconButton(
+        string name,
+        Transform original,
+        Transform parent,
+        Action<Transform> triggerAction,
+        Stream buttonTexture,
+        string hoverText = null)
+        : Button(name, original, parent, triggerAction, hoverText)
+    {
+        protected override void createButtonInternal()
+        {
+            if (buttonTexture == null) return;
+            var oldSprite = buttonTransform.FindChild("Icon").GetComponent<Image>().sprite;
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage(buttonTexture.ReadBytes());
+            var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+                oldSprite.textureRectOffset);
+            buttonTransform.FindChild("Icon").GetComponent<Image>().sprite = sprite;
         }
     }
 }
